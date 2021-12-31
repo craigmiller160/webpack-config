1731be49c871dd408ad58ebb32b06d0ed3251c76
Craig <craigmiller160@gmail.com>
2021-12-31 09:12:16 -0500

    Fixed devServer config

--- modules/webpack.devServer.js
+++ modules/webpack.devServer.js
@@ -1,18 +1,11 @@
 const path = require('path');
-const { HotModuleReplacementPlugin } = require('webpack');
 
 module.exports = {
     devServer: {
-        contentBase: [
-            path.resolve(process.cwd(), 'build')
-        ],
         compress: true,
         hot: true,
         historyApiFallback: {
             index: process.env.WEBPACK_PUBLIC_PATH || '/'
         }
-    },
-    plugins: [
-        new HotModuleReplacementPlugin()
-    ]
+    }
 };
